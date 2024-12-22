8d7e3f1f50126f40cfbf7575004c997ed68d2242
OrhanHuseinbegovic <o.huseinbegovic@gmail.com>
2024-12-22 16:48:46 +0100

    Added new .sol backend

--- backend/deccentralizedVotingSystem.sol
+++ backend/deccentralizedVotingSystem.sol
@@ -20,6 +20,7 @@ contract VotingSystem{
     struct VotingPoll {
         uint256 id;
         string name;
+        string description;
         address votePublisher;
         uint256 timeLeft;
         Status status;
@@ -85,6 +86,11 @@ contract VotingSystem{
         _;
     }
 
+    modifier isNotPending(uint256 _votingPollID) {
+        require(votingPolls[_votingPollID].status != Status.Pending, "Poll is pending!");
+        _;
+    }
+
     /*
         Function to check if the provided address exists inside
         of the addresses array, if yes, it will be logged, else
@@ -92,21 +98,23 @@ contract VotingSystem{
         will be logged. User type will be selected on 
     */
     function checkAddress(address _address, UserType _userType) public {
-        if (userRoles[_address] != UserType.Unassigned) { 
-            emit userLoggedIn(_address, userRoles[_address], block.timestamp);
-        } else {
+        if (userRoles[_address] == UserType.Unassigned) { 
             addresses.push(_address);
             userRoles[_address] = _userType;
             emit addedNewUser(_address, block.timestamp);
+            emit userLoggedIn(_address, userRoles[_address], block.timestamp);
+        } else {
+            userRoles[_address] = _userType;
             emit userLoggedIn(_address, _userType, block.timestamp);
         }
     }
 
-    function createPoll(string memory _name, uint256 _timeLeft) onlyVotePublisher public {
+    function createPoll(string memory _name, string memory _description,uint256 _timeLeft) onlyVotePublisher public {
         require(_timeLeft >= 43200, "Duration of the Voting Poll must be bigger or equal to 12h !");
         VotingPoll memory votingPoll;
         votingPoll.id = votingPollCounterID;
         votingPoll.name = _name;
+        votingPoll.description = _description;
         votingPoll.votePublisher = msg.sender;
         votingPoll.timeLeft = _timeLeft;
         votingPoll.status = Status.Pending;
@@ -131,7 +139,6 @@ contract VotingSystem{
         mapping(uint256 => VotingPoll) public votingPolls;
     */
     function deletePendingPoll(uint256 _votingPollID) onlyVotePublisher onlyPollOwner(_votingPollID) public returns (string memory) {
-        endVotingPolls();
         require(hasStarted[_votingPollID] == false, "Voting poll has started, can't delete poll!");
         require(votingPolls[_votingPollID].status == Status.Pending, "Voting poll has finished or started, can't delete poll now!");
         require(isFinished[_votingPollID] == false, "ERROR: Voting poll is ACTIVE!");
@@ -162,28 +169,33 @@ contract VotingSystem{
         hasStarted[_votingPollID] = true;
     }
 
-    function getVotingPollTime(uint256 _votingPollID) public view returns (uint256){
-        return votingPolls[_votingPollID].timeLeft;
-    }
-
     function getVotingPollName(uint256 _votingPollID) public view returns (string memory){
         return votingPolls[_votingPollID].name;
     }
 
-    /*
-        This function should be called by the sistem every XX minutes
-        to check if the timeleft for any of the Voting polls is 0,
-        if yes, to set the status do finished.
+    function getVotingPollDescription(uint _votingPollID) public view returns (string memory) {
+        return votingPolls[_votingPollID].description;
+    }
 
-        This function should be calle every time any function is called on the webserver for convenience
+    function getVotingPollTime(uint256 _votingPollID) public view returns (uint256){
+        return votingPolls[_votingPollID].timeLeft;
+    }
+
+    
+
+    /*
+        This function should be called every time any function is called on the webserver for convenience,
+        but we can't do this since every transaction then will have a fee. So, only
+        reasonable solution is to CALL this function whenever
+        any of the users wan't to vote for a certain option in a poll. So
+        firstly call this functio, then check if the poll has been ended, if not, 
+        vote, else, revert the function.
     */
-    function endVotingPolls() private {
-        for(uint256 i=0; i<votingPollCounterID; i++){
-            if(block.timestamp >= votingPolls[i].timeLeft){
-                votingPolls[i].status = Status.Finished;
-                hasStarted[i] = false; 
-                isFinished[i] = true;
-            }
+    function endVotingPoll(uint256 _votingPollID) private {
+        if(block.timestamp >= votingPolls[_votingPollID].timeLeft){
+            votingPolls[_votingPollID].status = Status.Finished;
+            hasStarted[_votingPollID] = false; 
+            isFinished[_votingPollID] = true;
         }
     } 
 
@@ -197,7 +209,6 @@ contract VotingSystem{
         3. Retrieve all FINISHED polls for VOTE PUBLISHER
     */
 
-    // Make a function to make all polls twhose time is up to be finished
     function getAllActivePollsVP() public view onlyVotePublisher returns(VotingPoll[] memory){
         uint256 activeCount = 0;
 
@@ -290,7 +301,7 @@ contract VotingSystem{
             5. On frontend side do the logic -> vote count of option / total vote count * 100 (rounded to 2) 
                = will get me desired percentage
     */
-    function getStatistics(uint256 _votingPollID) public view returns (VotingOption[] memory, uint256){
+    function getStatistics(uint256 _votingPollID) public view isNotPending(_votingPollID) returns (VotingOption[] memory, uint256){
         uint256 totalVoteCount = 0;
         for(uint256 i = 0; i<votingOptionsByPoll[_votingPollID].length; i++){
             totalVoteCount += votingOptionsByPoll[_votingPollID][i].voteCount;
@@ -308,8 +319,8 @@ contract VotingSystem{
         1. Retrieve all ACTIVE - NON VOTED polls for VOTER which is not the 
             polls Vote Publisher
     */
-    function getAllActivePollsVOTER() public isVoter returns (VotingPoll[] memory){
-        endVotingPolls();
+    function getAllActivePollsVOTER() public view isVoter returns (VotingPoll[] memory){
+        //Delete endVotingPolls() function from here
         uint256 activePolls = 0;
         for(uint256 i = 0; i<votingPollCounterID; i++){
             if(hasVoted[msg.sender][i] == false && isPublisher(i) == false && votingPolls[i].status == Status.Active){
@@ -326,7 +337,40 @@ contract VotingSystem{
         return activeVotingPolls;
     }
 
-    //Function to retrieve all voting options for regular user of a voting poll
+    function getAllVotedPollsVOTER() public view isVoter returns (VotingPoll[] memory) {
+        uint256 activePolls = 0;
+        for(uint256 i = 0; i<votingPollCounterID; i++){
+            if(hasVoted[msg.sender][i] == true && votingPolls[i].status == Status.Active){
+                activePolls++;
+            }
+        }
+        VotingPoll[] memory activeVotingPolls = new VotingPoll[](activePolls);
+        uint256 index = 0;
+        for(uint256 i = 0; i<votingPollCounterID; i++){
+            if(hasVoted[msg.sender][i] == true && votingPolls[i].status == Status.Active){
+                activeVotingPolls[index++] = votingPolls[i];
+            }
+        }
+        return activeVotingPolls;
+    }
+
+    function getAllFinishedPollsVOTER() public view isVoter returns (VotingPoll[] memory) {
+        uint256 finishedPolls = 0;
+        for(uint256 i = 0; i<votingPollCounterID; i++){
+            if(votingPolls[i].status == Status.Finished){
+                finishedPolls++;
+            }
+        }
+        VotingPoll[] memory finishedVotingPolls = new VotingPoll[](finishedPolls);
+        uint256 index = 0;
+        for(uint256 i = 0; i<votingPollCounterID; i++){
+            if(votingPolls[i].status == Status.Finished){
+                finishedVotingPolls[index++] = votingPolls[i];
+            }
+        }
+        return finishedVotingPolls;
+    }
+
     function getAllVotingOptions(uint256 _votingPollID) public view returns(VotingOption[] memory) {
         return votingOptionsByPoll[_votingPollID];  
     }
@@ -339,7 +383,10 @@ contract VotingSystem{
         //Update that the user has voted
         //Emit that the user has voted
         //update the vote count for that voting poll
-        endVotingPolls();
+        if(block.timestamp >= votingPolls[_votingPollID].timeLeft){
+            endVotingPoll(_votingPollID);
+        }
+        require(votingPolls[_votingPollID].status == Status.Active, "Can't vote for a finished poll!");
         votingOptionsByPoll[_votingPollID][_votingOptionID].voteCount++;
         hasVoted[msg.sender][_votingPollID] = true;
         emit newVote(msg.sender, _votingPollID, block.timestamp);
